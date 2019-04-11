419b0654c03b469e9cf4795014b42438cdd554cd
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-11 09:17:04 +0200

    md

--- CardioanalisiLibrary/DataCardio.cs
+++ CardioanalisiLibrary/DataCardio.cs
@@ -8,14 +8,26 @@ namespace CardioanalisiLibrary
 {
     public class DataCardio
     {
-        public static double BatMax(int eta)
+        public static double BatMax(int eta, int ris)
         {
-            return (220 - eta) * 0.9;
+            if (eta <= 0 || eta > 120) return -1;
+            else return (220 - eta) * 0.9;
         }
 
-        public static double BatMin(int eta)
+        public static double BatMin(int eta, int ris)
         {
-            return (220 - eta) * 0.7;
+            if (eta <= 0 || eta > 120) return -1;
+            else return (220 - eta) * 0.7;
         }
+
+        public static string Valori(int freq, string ris)
+        {
+            if (freq < 60) return "Bradicardia";
+            if (freq >= 60 || freq <= 100) return "Normale";
+            if (freq > 100) return "Tachicardia";
+            if (freq <= 0) return "E";
+        }
+            
+
     }
 }
--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -7,8 +7,35 @@ namespace DataCardio.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow(-1,-1)]
+        [DataRow(0,-1)]
+        [DataRow(121,-1)]
+        [DataRow(20, 180)]
+        [DataRow(53, 132.3)]
+        [DataRow(62, 142.2)]
+
+        public void TestMethodBatMax(int eta, int ris)
         {
+            double att = CardioanalisiLibrary.DataCardio.BatMax(eta, ris);
+
+            Assert.AreEqual(att, ris);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(-1, -1)]
+        [DataRow(0, -1)]
+        [DataRow(121, -1)]
+        [DataRow(20, 140)]
+        [DataRow(53, 116.9)]
+        [DataRow(62, 110.6)]
+
+        public void TestMethodBatMin(int eta, int ris)
+        {
+            double att = CardioanalisiLibrary.DataCardio.BatMax(eta, ris);
+
+            Assert.AreEqual(att, ris);
         }
     }
 }
