9e7f7b27c91be4214dc53b7482a3db819abfe85b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-11 08:48:24 +0200

    2

--- CardioanalisiLibrary/DataCardio.cs
+++ CardioanalisiLibrary/DataCardio.cs
@@ -8,6 +8,14 @@ namespace CardioanalisiLibrary
 {
     public class DataCardio
     {
-         
+        public static double BatMax(int eta)
+        {
+            return (220 - eta) * 0.9;
+        }
+
+        public static double BatMin(int eta)
+        {
+            return (220 - eta) * 0.7;
+        }
     }
 }
