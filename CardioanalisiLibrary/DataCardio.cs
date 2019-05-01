bf78f2589c44d7590347ff3ccce866829258c4c6
Sandro Meloni <sandromeloni02@gmail.com>
2019-05-01 20:55:15 +0200

    iniziato esercizio 5

--- CardioanalisiLibrary/DataCardio.cs
+++ CardioanalisiLibrary/DataCardio.cs
@@ -8,18 +8,20 @@ namespace CardioanalisiLibrary
 {
     public class DataCardio
     {
-        public static double BatMax(int eta, double ris)
+        //punto 1 calcolo i battimi massimi e minimi da mantenere in allenamento
+        public static double BatMax(int eta, int ris)
         {
             if (eta <= 0 || eta > 120) return -1;
             else return (220 - eta) * 0.9;
         }
 
-        public static double BatMin(int eta, double ris)
+        public static double BatMin(int eta, int ris)
         {
             if (eta <= 0 || eta > 120) return -1;
             else return (220 - eta) * 0.7;
         }
 
+        //punto 2 interpreto i valori dei battiti a riposo
         public static string Valori(int freq, string ris)
         {
             if (freq <= 0) return "Errore";
@@ -29,6 +31,7 @@ namespace CardioanalisiLibrary
             else return "Errore";
         }
 
+        //punto 3 calcolo le calorie bruciatedurante un esercizio
         public static double CalorieBruciate(int a, int p, int f, int t, string ses, int ris)
         {
             if (f <= 0 || p <= 0 || a <= 0 || t <= 0) return -1;
@@ -37,11 +40,58 @@ namespace CardioanalisiLibrary
             else return -1;
         }
 
+        //punto 4 calcolo la spesa energetica di quando si corre o cammina
         public static double SpesaEnergetica(int km, int p, string coc, double ris)
         {
             if (km <= 0 || p <= 0 || coc != "camminata" && coc != "corsa") return -1;
             else if (coc == "corsa") return 0.9 * km * p;
             else return 0.5 * km * p;
-        } 
+        }
+
+        //punto 5A calcolo la media giornaliera dei battiti
+        public static double MediaGiornalieraBattiti(List<int> ListaFrequenzaQuotidiano)
+        {
+            double media = 0;
+            bool err = false;
+
+            for (int i = 0; i < ListaFrequenzaQuotidiano.Count; i++)
+            {
+                media = media + ListaFrequenzaQuotidiano[i];
+
+                if (ListaFrequenzaQuotidiano[i] <= 0) err = true;
+            }
+            media = media / ListaFrequenzaQuotidiano.Count;
+
+            if (err == true) return -1;
+            else return media;
+        }
+
+        //punto 5B calcolo il battito cardiaco a riposo
+        public static string BattitoCardiacoRiposo(int freq, string ris)
+        {
+            if (freq <= 0) return "Errore";
+            if (freq < 60) return "Bradicardia";
+            else if (freq >= 60 && freq <= 100) return "Normale";
+            else if (freq > 100) return "Tachicardia";
+            else return "Errore";
+        }
+
+        //punto 5C calcolo la variabilita dei battiti
+        //public static double VariabilitaBattito(List<double> TempoTraDueBattiti)
+        //{            
+        //}
+
+        //punto 5D ordino i battiti durante la giornata
+        public static List<int> OrdineCrescenteBattiti(List<int> ListaFrequenzaGiornata)
+        {
+            for (int i = 0; i < ListaFrequenzaGiornata.Count; i++)
+            {
+                if (ListaFrequenzaGiornata[i] <= 0 || ListaFrequenzaGiornata[i] > 220) ListaFrequenzaGiornata[i] = -1;
+            }
+
+            ListaFrequenzaGiornata.Sort();
+            return ListaFrequenzaGiornata;
+        }
+
     }
 }
--- DataCardio.Test/UnitTest1.cs
+++ DataCardio.Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DataCardio.Test
@@ -6,38 +7,41 @@ namespace DataCardio.Test
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        [DataTestMethod]
-        [DataRow(-1,-1)]
-        [DataRow(0,-1)]
-        [DataRow(121,-1)]
-        [DataRow(20, 180.0)]
-        [DataRow(53, 132.3)]
-        [DataRow(62, 142.2)]
-
-        public void TestMethodBatMax(int eta, double ris)
-        {
-            double att = CardioanalisiLibrary.DataCardio.BatMax(eta, ris);
-
-            Assert.AreEqual(att, ris);
-        }
-
+        //test punto 1
         [TestMethod]
         [DataTestMethod]
         [DataRow(-1, -1)]
         [DataRow(0, -1)]
         [DataRow(121, -1)]
-        [DataRow(20, 140.0)]
-        [DataRow(53, 116.9)]
-        [DataRow(62, 110.6)]
+        [DataRow(20, 180)]
+        [DataRow(53, 132.3)]
+        [DataRow(62, 142.2)]
 
-        public void TestMethodBatMin(int eta, double ris)
+        public void TestMethodBatMax(int eta, int ris)
         {
             double att = CardioanalisiLibrary.DataCardio.BatMax(eta, ris);
 
             Assert.AreEqual(att, ris);
         }
 
+        //test punto 1
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(-1, -1)]
+        [DataRow(0, -1)]
+        [DataRow(121, -1)]
+        [DataRow(20, 140)]
+        [DataRow(53, 116.9)]
+        [DataRow(62, 110.6)]
+
+        public void TestMethodBatMin(int eta, int ris)
+        {
+            double att = CardioanalisiLibrary.DataCardio.BatMax(eta, ris);
+
+            Assert.AreEqual(att, ris);
+        }
+
+        //test punto 2
         [TestMethod]
         [DataTestMethod]
         [DataRow(-1, "Errore")]
@@ -54,6 +58,7 @@ namespace DataCardio.Test
             Assert.AreEqual(att, ris);
         }
 
+        //test punto 3
         [TestMethod]
         [DataTestMethod]
         [DataRow(0, 0, 0, 0, "M", -1)]
@@ -67,17 +72,70 @@ namespace DataCardio.Test
             Assert.AreEqual(att, ris);
         }
 
+        //test punto 4
         [TestMethod]
         [DataTestMethod]
+        [DataRow(0, 0, "a", -1)]
         [DataRow(1, 60, "corsa", 54)]
-        [DataRow(2, 50, "camminata", 50)]
-        [DataRow(0, 0, "corsa", -1)]
+        [DataRow(2, 60, "camminata", 60)]
 
-        public void TestMethodSpesaEnergetica(int km, int p, string coc, int ris)
+        public void TestMethodSpesaEnergetica(int km, int p, string coc, double ris)
         {
             double att = CardioanalisiLibrary.DataCardio.SpesaEnergetica(km, p, coc, ris);
 
             Assert.AreEqual(att, ris);
+        }
+
+        //test punto 5A
+        [TestMethod]
+
+        public void TestMethodMediaGiornalieraBattiti1()
+        {
+            List<int> ListaFreq = new List<int>() { 0, 800, -20 };
+            double risp = CardioanalisiLibrary.DataCardio.MediaGiornalieraBattiti(ListaFreq);
+
+            Assert.AreEqual(-1, risp);
+        }
+
+        [TestMethod]
+
+        public void TestMethodMediaGiornalieraBattiti2()
+        {
+            List<int> ListaFreq = new List<int>() { 100, 200, 150 };
+            double risp = CardioanalisiLibrary.DataCardio.MediaGiornalieraBattiti(ListaFreq);
+
+            Assert.AreEqual(150, risp);
+        }
+
+        //test punto 5B
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(-1, "Errore")]
+        [DataRow(0, "Errore")]
+        [DataRow(20, "Bradicardia")]
+        [DataRow(60, "Normale")]
+        [DataRow(100, "Normale")]
+        [DataRow(120, "Tachicardia")]
+
+        public void TestMethodBattitoCardiacoRiposo(int freq, string ris)
+        {
+            string att = CardioanalisiLibrary.DataCardio.Valori(freq, ris);
+
+            Assert.AreEqual(att, ris);
+        }
+
+        //test punto 5C
+
+        //test punto 5D
+        [TestMethod]
+
+        public void TestMethodOrdineCrescenteBattiti()
+        {
+            List<int> ListaFreq = new List<int>() { 60, 100, 98, 73, 86, 50 };
+            List<int> risp = CardioanalisiLibrary.DataCardio.OrdineCrescenteBattiti(ListaFreq);
+            List<int> att = new List<int>() { 50, 60, 73, 86, 98, 100 };
+
+            Assert.AreEqual(att, risp);
         }
     }
 }
